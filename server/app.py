f02240ce3e21e1921cc02e22f2088824059114d4
barakatimothy <timothybaraka39@gmail.com>
2023-09-27 13:05:55 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_encoder.compact = False  # Fix: It should be app.json_encoder.compact
 
 migrate = Migrate(app, db)
 
@@ -20,15 +20,35 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
-@app.route('/articles')
-def index_articles():
-
-    pass
-
-@app.route('/articles/<int:id>')
+@app.route('/articles/<int:id>', methods=['GET'])
 def show_article(id):
+    # Set the initial value to 0 if it's the first request for this user
+    session['page_views'] = session.get('page_views', 0)
 
-    pass
+    # Increment page_views for every request
+    session['page_views'] += 1
+
+    # Check if the user has viewed 3 or fewer pages
+    if session['page_views'] <= 3:
+        # Get the article data (you need to implement this)
+        article = Article.query.get(id)
+        if article:
+            # Render JSON response with article data
+            return jsonify({
+                'message': f'Successfully viewed article {id}',
+                'article': {
+                    'id': article.id,
+                    'title': article.title,
+                    'content': article.content
+                    # Add other fields as needed
+                }
+            }), 200
+        else:
+            # Handle the case where the article with the given id is not found
+            return {'message': '404: Article not found'}, 404
+    else:
+        # Render JSON response with an error message for exceeding the pageview limit
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
 if __name__ == '__main__':
     app.run(port=5555)
